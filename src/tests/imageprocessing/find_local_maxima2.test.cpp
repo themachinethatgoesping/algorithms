5e151ff95f2793fd8375a7d2ad58152f8d58959a
Peter Urban <peter.urban@ugent.be>
2025-03-20 09:24:20 +0100

    test that find_local_maxima return type is compatible with xt::index_view

--- src/tests/imageprocessing/find_local_maxima2.test.cpp
+++ src/tests/imageprocessing/find_local_maxima2.test.cpp
@@ -25,10 +25,18 @@ TEST_CASE("find_local_maxima2 in 3D", TESTTAG)
         return static_cast<float>(std::rand()) / (static_cast<float>(RAND_MAX / 1.99f));
     });
 
-    struct Peak { int64_t x, y, z; float value; };
-    std::vector<Peak> peaks = { {10,20,30,6.5f},{25,25,25,7.2f},{30,15,35,8.8f},
-                                {35,35,20,9.1f},{40,40,45,10.5f} };
-    for (auto& p : peaks) data(p.x, p.y, p.z) += p.value;
+    struct Peak
+    {
+        int64_t x, y, z;
+        float   value;
+    };
+    std::vector<Peak> peaks = { { 10, 20, 30, 6.5f },
+                                { 25, 25, 25, 7.2f },
+                                { 30, 15, 35, 8.8f },
+                                { 35, 35, 20, 9.1f },
+                                { 40, 40, 45, 10.5f } };
+    for (auto& p : peaks)
+        data(p.x, p.y, p.z) += p.value;
 
     auto maxima = find_local_maxima2(data, 2.0f);
     REQUIRE(maxima.size() == peaks.size());
@@ -38,7 +46,11 @@ TEST_CASE("find_local_maxima2 in 3D", TESTTAG)
     {
         auto val = data(maxima[i][0], maxima[i][1], maxima[i][2]);
         info_str += fmt::format("Peak {}: x={}, y={}, z={}, value={}\n",
-                                i, maxima[i][0], maxima[i][1], maxima[i][2], val);
+                                i,
+                                maxima[i][0],
+                                maxima[i][1],
+                                maxima[i][2],
+                                val);
     }
     INFO(info_str);
 
@@ -52,12 +64,22 @@ TEST_CASE("find_local_maxima2 in 3D", TESTTAG)
     for (size_t idx = 0; idx < sorted_indices.size(); ++idx)
     {
         const auto& peak = peaks[idx];
-        const auto i     = sorted_indices[idx];
+        const auto  i    = sorted_indices[idx];
         REQUIRE(maxima[i][0] == peak.x);
         REQUIRE(maxima[i][1] == peak.y);
         REQUIRE(maxima[i][2] == peak.z);
         auto val = data(peak.x, peak.y, peak.z);
         REQUIRE(val == Approx(peak.value).margin(2.0));
+    }
+
+    //check that the returned maxima type is compatible with xt::index_view
+    auto lm_values = xt::index_view(data, maxima);
+    for (size_t idx = 0; idx < sorted_indices.size(); ++idx)
+    {
+        INFO(fmt::format("lm_values[{}]: {} peaks[{}].value: {}", idx, lm_values[idx], idx, peaks[idx].value));
+        REQUIRE(lm_values[idx] == Approx(peaks[idx].value).margin(2.0));
+        REQUIRE(lm_values[idx] == Approx(data(peaks[idx].x,peaks[idx].y,peaks[idx].z)).margin(0.1));
+
     }
 }
 
@@ -70,10 +92,16 @@ TEST_CASE("find_local_maxima2 in 2D", TESTTAG)
         return static_cast<float>(std::rand()) / (static_cast<float>(RAND_MAX / 1.99f));
     });
 
-    struct Peak { int64_t x, y; float value; };
-    std::vector<Peak> peaks = { {10,20,6.5f},{25,25,7.2f},{30,15,8.8f},
-                                {35,35,9.1f},{40,40,10.5f} };
-    for (auto& p : peaks) data(p.x, p.y) += p.value;
+    struct Peak
+    {
+        int64_t x, y;
+        float   value;
+    };
+    std::vector<Peak> peaks = {
+        { 10, 20, 6.5f }, { 25, 25, 7.2f }, { 30, 15, 8.8f }, { 35, 35, 9.1f }, { 40, 40, 10.5f }
+    };
+    for (auto& p : peaks)
+        data(p.x, p.y) += p.value;
 
     auto maxima = find_local_maxima2(data, 2.0f);
     REQUIRE(maxima.size() == peaks.size());
@@ -82,8 +110,8 @@ TEST_CASE("find_local_maxima2 in 2D", TESTTAG)
     for (size_t i = 0; i < maxima.size(); ++i)
     {
         auto val = data(maxima[i][0], maxima[i][1]);
-        info_str += fmt::format("Peak {}: x={}, y={}, value={}\n",
-                                i, maxima[i][0], maxima[i][1], val);
+        info_str +=
+            fmt::format("Peak {}: x={}, y={}, value={}\n", i, maxima[i][0], maxima[i][1], val);
     }
     INFO(info_str);
 
@@ -97,11 +125,21 @@ TEST_CASE("find_local_maxima2 in 2D", TESTTAG)
     for (size_t idx = 0; idx < sorted_indices.size(); ++idx)
     {
         const auto& peak = peaks[idx];
-        const auto i     = sorted_indices[idx];
+        const auto  i    = sorted_indices[idx];
         REQUIRE(maxima[i][0] == peak.x);
         REQUIRE(maxima[i][1] == peak.y);
         auto val = data(peak.x, peak.y);
         REQUIRE(val == Approx(peak.value).margin(2.0));
+    }
+
+    //check that the returned maxima type is compatible with xt::index_view
+    auto lm_values = xt::index_view(data, maxima);
+    for (size_t idx = 0; idx < sorted_indices.size(); ++idx)
+    {
+        INFO(fmt::format("lm_values[{}]: {} peaks[{}].value: {}", idx, lm_values[idx], idx, peaks[idx].value));
+        REQUIRE(lm_values[idx] == Approx(peaks[idx].value).margin(2.0));
+        REQUIRE(lm_values[idx] == Approx(data(peaks[idx].x,peaks[idx].y)).margin(0.1));
+
     }
 }
 
@@ -114,9 +152,16 @@ TEST_CASE("find_local_maxima2 in 1D", TESTTAG)
         return static_cast<float>(std::rand()) / (static_cast<float>(RAND_MAX / 1.99f));
     });
 
-    struct Peak { int64_t x; float value; };
-    std::vector<Peak> peaks = { {10,6.5f},{25,7.2f},{30,8.8f},{35,9.1f},{40,10.5f} };
-    for (auto& p : peaks) data(p.x) += p.value;
+    struct Peak
+    {
+        int64_t x;
+        float   value;
+    };
+    std::vector<Peak> peaks = {
+        { 10, 6.5f }, { 25, 7.2f }, { 30, 8.8f }, { 35, 9.1f }, { 40, 10.5f }
+    };
+    for (auto& p : peaks)
+        data(p.x) += p.value;
 
     auto maxima = find_local_maxima2(data, 2.0f);
     REQUIRE(maxima.size() == peaks.size());
@@ -139,9 +184,19 @@ TEST_CASE("find_local_maxima2 in 1D", TESTTAG)
     for (size_t idx = 0; idx < sorted_indices.size(); ++idx)
     {
         const auto& peak = peaks[idx];
-        const auto i     = sorted_indices[idx];
+        const auto  i    = sorted_indices[idx];
         REQUIRE(maxima[i] == peak.x);
         auto val = data(peak.x);
         REQUIRE(val == Approx(peak.value).margin(2.0));
+    }
+
+    //check that the returned maxima type is compatible with xt::index_view
+    auto lm_values = xt::index_view(data, maxima);
+    for (size_t idx = 0; idx < sorted_indices.size(); ++idx)
+    {
+        INFO(fmt::format("lm_values[{}]: {} peaks[{}].value: {}", idx, lm_values[idx], idx, peaks[idx].value));
+        REQUIRE(lm_values[idx] == Approx(peaks[idx].value).margin(2.0));
+        REQUIRE(lm_values[idx] == Approx(data(peaks[idx].x)).margin(0.1));
+
     }
 }
