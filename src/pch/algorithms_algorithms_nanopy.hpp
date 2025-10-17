8774c55bdaaf3a4e29e57460b14a44e3dc2ee327
Peter Urban <peter.urban@ugent.be>
2025-10-17 22:47:21 +0200

    update pch (win fix)

--- src/pch/algorithms_algorithms_nanopy.hpp
+++ src/pch/algorithms_algorithms_nanopy.hpp
@@ -2,7 +2,9 @@
 // Target: algorithms:algorithms_nanopy
 #pragma once
 
-#include <nanobind/nanobind.h>
+#if !defined(__WIN32__) && !defined(_WIN32) && !defined(__WIN64__) && !defined(_WIN64)
+    #include <nanobind/nanobind.h>
+#endif
 #include <nanobind/stl/string.h>
 #include <xtensor/containers/xtensor.hpp>
 #include <nanobind/stl/vector.h>
