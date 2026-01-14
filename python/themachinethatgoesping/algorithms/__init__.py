edc939d111985d9b7a493d545ce09b84c71daa55
Peter Urban <peter.urban@ugent.be>
2026-01-14 23:21:29 +0100

    some new parameter functions

--- python/themachinethatgoesping/algorithms/__init__.py
+++ python/themachinethatgoesping/algorithms/__init__.py
@@ -9,6 +9,8 @@ from themachinethatgoesping.algorithms_nanopy import *  # flake8: noqa
 from themachinethatgoesping.tools._submodule_helper import expose_submodules
 from themachinethatgoesping import algorithms_nanopy
 
+from . import backscattermodels
+
 globals().update(expose_submodules(algorithms_nanopy, 'themachinethatgoesping.algorithms'))
 del algorithms_nanopy, expose_submodules
 
---
+++ python/themachinethatgoesping/algorithms/backscattermodels/gasbubbles/__init__.py
@@ -0,0 +1,3 @@
+from .parameters import *  # noqa: F403
+from .thuraisingham import *  # noqa: F403
+from .li import *  # noqa: F403
